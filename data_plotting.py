f14ef502392298513880eca011ba9bf9e8aa8c7d
Alexey <alexey_kuzminykh@mail.ru>
2024-12-17 23:18:41 +0300

    Задача №1. Реализовать функционал: Вывод средней цены за период

--- data_plotting.py
+++ data_plotting.py
@@ -29,3 +29,15 @@ def create_and_save_plot(data, ticker, period, filename=None):
 
     plt.savefig(filename)
     print(f"График сохранен как {filename}")
+
+
+def calculate_and_display_average_price(data):
+    """
+     Функция вычисляет и выводит среднюю цену закрытия акций за весь период.
+    :param data: DataFrame с историей цен акций.
+    """
+    if 'Close' in data:
+        average_price = data['Close'].mean()
+        print(f"Средняя цена закрытия акций за указанный период: {average_price:.2f}")
+    else:
+        print("Колонка 'Close' отсутствует в данных.")
--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import data_download as dd
 import data_plotting as dplt
 
-
 def main():
     print("Добро пожаловать в инструмент получения и построения графиков биржевых данных.")
     print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
     print("Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
 
-    ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
+    ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc): »")
     period = input("Введите период для данных (например, '1mo' для одного месяца): ")
 
-    # Fetch stock data
+    # Получаем данные о запасах
     stock_data = dd.fetch_stock_data(ticker, period)
 
-    # Add moving average to the data
+    # Добавляем скользящее среднее в данные
     stock_data = dd.add_moving_average(stock_data)
 
-    # Plot the data
+    # Выводим среднюю цену за период
+    dplt.calculate_and_display_average_price(stock_data)
+
+    # Строим график и сохраняем его
     dplt.create_and_save_plot(stock_data, ticker, period)
 
 
